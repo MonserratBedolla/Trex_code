a4a5ff84e825305932e97155b8d633dba22e07ab
Eric70238 <117419646+Eric70238@users.noreply.github.com>
2022-11-29 20:56:58 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,166 @@
+
+var trex ,trex_running, trex_collided;
+var ground, groundImage, invisibleGround;
+var cloudsGroup, cloud, cloudImage;
+var obstacle;
+var obstaclesGroup, obstacle1, obstacle2, obstacle3, obstacle4, obstacle5, obstacle6;
+var score = 0;
+var PLAY = 1;
+var END = 0;
+var gameState = PLAY;
+var restart;
+var gameOver;
+var gameOverImg, restartImg
+
+
+function preload(){
+  trex_running = loadAnimation("trex1.png","trex3.png","trex4.png");
+  trex_collided = loadAnimation("trex_collided.png");
+  groundImage = loadImage("ground2.png")
+  cloudImage = loadImage("cloud.png")
+  obstacle1 = loadImage("obstacle1.png")
+  obstacle2 = loadImage("obstacle2.png")
+  obstacle3 = loadImage("obstacle3.png")
+  obstacle4 = loadImage("obstacle4.png")
+  obstacle5 = loadImage("obstacle5.png")
+  obstacle6 = loadImage("obstacle6.png")
+  restartImg = loadImage("restart.png")
+  gameOverImg = loadImage("gameOver.png")
+}
+
+function setup(){
+  createCanvas(600,200)
+  
+  //crear sprite de Trex
+ trex = createSprite (50, 160, 20, 50);
+ trex.addAnimation("running", trex_running);
+ trex.scale = 0.5;
+ trex.addAnimation("collided", trex_collided)
+ //sprite del suelo
+ ground = createSprite (200,180,400,20);
+ ground.addImage("ground",groundImage)
+ ground.x = ground.width/2;
+
+ restart= createSprite (300,120);
+ restart.addImage(restartImg);
+ restart.scale=0.5;
+
+ gameOver = createSprite (300,75);
+ gameOver.addImage(gameOverImg);
+ gameOver.scale=0.8
+
+ //crear sprite de suelo invisible
+ invisibleGround = createSprite(200,190,400,10);
+ invisibleGround.visible = false;
+
+  //crear grupos de nubes y obstaculos
+  obstaclesGroup = createGroup();
+  cloudsGroup = createGroup ();
+
+  trex.setCollider("circle",0,0,40);
+  trex.debug=true;
+}
+
+function draw(){
+  background(180)
+
+  //Mostrar puntuación
+  text("Puntuación: "+score,500,50)
+
+  if(gameState===PLAY){
+
+    if(obstaclesGroup.isTouching(trex)){
+      gameState=END;
+    }
+
+    //agregar todo lo que pasa al jugar
+    if (ground.x<0)
+    {
+      ground.x = ground.width/2;
+    }
+   
+    ground.velocityX = -4;
+
+   //hacer que el Trex salte al presionar la barra espaciadora
+    if (keyDown("space")&& trex.y>=100)
+    {
+      trex.velocityY = -10;
+    } 
+
+    //creamos la gravedad
+    trex.velocityY = trex.velocityY+ 0.8;
+
+    trex.collide(invisibleGround);
+
+    restart.visible=false;
+    gameOver.visible=false;
+
+
+      score = score + Math.round(frameCount/60);
+
+  }
+  else if (gameState===END){
+    //agregar todo lo quepasa al perder
+    ground.velocityX=0;
+    cloudsGroup.setVelocityXEach(0);
+    obstaclesGroup.setVelocityXEach(0);
+    restart.visible=true;
+    gameOver.visible=true;
+    trex.velocityY=0
+    trex.changeAnimation("collided",trex_collided);
+    obstaclesGroup.setLifetimeEach(-1);
+    cloudsGroup.setLifetimeEach(-1);
+  }
+
+  spawnClouds();
+  spawnObstacles();
+  drawSprites();
+}
+
+function spawnClouds(){
+
+  if (frameCount % 60 === 0){
+  cloud=createSprite (600,100,40,10);
+  cloud.velocityX=-3;
+  cloud.addImage(cloudImage);
+  cloud.y=Math.round(random(10,80));
+  cloud.scale = 0.7;
+  cloud.depth=trex.depth;
+  trex.depth=trex.depth+1;
+  cloud.lifetime=200;
+  cloudsGroup.add(cloud);
+  }
+  
+
+  console.warn(frameCount);
+  
+}
+function spawnObstacles(){
+  if (frameCount % 60 === 0){
+    obstacle = createSprite (600,170,10,40);
+    obstacle.velocityX=-6;
+    var rand=Math.round(random(1,6));
+    
+   switch(rand){
+    case 1: obstacle.addImage(obstacle1);
+    break;
+    case 2: obstacle.addImage(obstacle2);
+    break;
+    case 3: obstacle.addImage(obstacle3);
+    break;
+    case 4: obstacle.addImage(obstacle4);
+    break;
+    case 5: obstacle.addImage(obstacle5);
+    break;
+    case 6: obstacle.addImage(obstacle6);
+    break;
+    
+   }
+   obstacle.scale=0.5;
+   obstacle.lifetime=300;
+             
+   //agregar grupo
+   obstaclesGroup.add(obstacle);
+
+  }
+}
